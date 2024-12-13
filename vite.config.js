2d8c802e6db821d4da5f6a2c03f48988ef77d1e3
ManziPatrick <alainmanzi71gmail.com>
2024-12-13 12:21:15 +0200

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,13 @@
-import { defineConfig } from "vite"; 
-import react from "@vitejs/plugin-react"; 
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 import tailwindcss from "tailwindcss";
 
-// https://vitejs.dev/config/ 
-export default defineConfig({   
-    plugins: [react()],   
-    css: {
-        postcss: {
-            plugins: [tailwindcss()],
-        },   
-    }, 
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  css: {
+    postcss: {
+      plugins: [tailwindcss],
+    },
+  },
 });
